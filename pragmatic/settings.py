42f399c05291e53504c5a19b8c92229637bfbfce
yeeuniii <anna862700@gmail.com>
2024-01-19 20:09:05 +0900

    feat: Static 설정 및 CSS 파일 분리

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -129,7 +129,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
