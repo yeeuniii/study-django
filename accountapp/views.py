15354f6f505c12ed3d5376fec3cc646c376076d4
yeeuniii <anna862700@gmail.com>
2024-01-19 21:37:25 +0900

    feat: POST method를 통한 data를 DB에 저장

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import HelloWorld
+
+
 # Create your views here.
 
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save() # DB에 HelloWorld 객체 저장
+
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+    return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
