bcd566b15f990fb4f162233e797a42653f170b8e
yeeuniii <anna862700@gmail.com>
2024-01-25 17:24:02 +0900

    feat: accountapp 가입 기능 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -12,13 +15,20 @@ def hello_world(request):
     if request.method == "POST":
         temp = request.POST.get('hello_world_input')
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save() # DB에 HelloWorld 객체 저장
+        if temp:
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save() # DB에 HelloWorld 객체 저장
 
 #        hello_world_list = HelloWorld.objects.all()
 #        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
-            # account 내 있는 hello world로 재접속하라
+                                                # account 내 있는 hello world로 재접속하라
     hello_world_list = HelloWorld.objects.all()
     return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
