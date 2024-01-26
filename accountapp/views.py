939a37ae5a25266c95a7dcc1f0058f47086fc9ed
yeeuniii <anna862700@gmail.com>
2024-01-26 14:31:50 +0900

    refactor: update&delete view context 객체 이름 수정 및 url 변경

--- accountapp/views.py
+++ accountapp/views.py
@@ -35,18 +35,20 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
-class AccountDetailView(DetailView):
+class AccountDetailView (DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
 class AccountUpdateView(UpdateView):
     model = User
+    context_object_name = 'target_user'
     form_class = UserUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
